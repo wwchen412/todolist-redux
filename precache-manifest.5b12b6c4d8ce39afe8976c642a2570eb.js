594dd6f9ddbf1edee28a587e238a118abddefb2d
william <wwchen412@gmail.com>
2020-05-28 16:47:08 +0800

    Updates

---
+++ precache-manifest.5b12b6c4d8ce39afe8976c642a2570eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a1bd25f2c83e21a4472b0529bf8529",
+    "url": "/todolist-redux/index.html"
+  },
+  {
+    "revision": "b27cdbb7f056760dc919",
+    "url": "/todolist-redux/static/css/main.cac14ed5.chunk.css"
+  },
+  {
+    "revision": "59bb27a9702f4775120b",
+    "url": "/todolist-redux/static/js/2.b46cb699.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist-redux/static/js/2.b46cb699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b27cdbb7f056760dc919",
+    "url": "/todolist-redux/static/js/main.319c0b79.chunk.js"
+  },
+  {
+    "revision": "c252e99b903e0ae1a605",
+    "url": "/todolist-redux/static/js/runtime-main.bd6c55bf.js"
+  }
+]);
